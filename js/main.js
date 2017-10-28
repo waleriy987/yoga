f595f364784c2d53682195a54773d1b8c11950a9
Ковтун Валерий <valera.kowtun@gmail.com>
2017-10-28 10:33:16 +0300

    last commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
 	$("#e0").select2();
-
 	$(".regular").slick({
         dots: true,
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1
       });
-
 	$(".paymentBig").slick({
 		  slidesToShow: 1,
 		  slidesToScroll: 1,
@@ -16,7 +14,6 @@ $(document).ready(function () {
 		  fade: true,
 		  asNavFor: '.paymentSmall'
       });
-
 	$(".paymentSmall").slick({
 		  slidesToShow: 1,
 		  slidesToScroll: 1,
@@ -24,58 +21,66 @@ $(document).ready(function () {
 		  centerMode: true,
 		  focusOnSelect: true
       });
-
-
 	$(window).scroll(function() {
-
 		var headerFixed = $(window).scrollTop();
-
 		if(headerFixed > 120){
 			$(".header-container").addClass("fixedHeader");
 		}else if(headerFixed < 120){
 			$(".header-container").removeClass("fixedHeader");
 		}
-		
+	});
+	$(document).on("mouseover", ".payment_card_box__item-btn", function() {
+		$(this).parent().siblings().children().css("color", "blue");
+		$(this).css("background-color", "blue");
+	});
+	$(document).on("mouseout", ".payment_card_box__item-btn", function() {
+		$(this).parent().siblings().children().css("color", "#fff");
+		$(this).css("background-color", "#5b6ceb");
 	});
 
 
+	$(document).on("click", ".header__open-mobile-menu", function () {
+		$(".mobail-menu-container").animate({left:"0%"}, 500);
+    });
 
-$(document).on("mouseover", ".payment_card_box__item-btn", function() {
-	$(this).parent().siblings().children().css("color", "blue");
-	$(this).css("background-color", "blue");
-});
-$(document).on("mouseout", ".payment_card_box__item-btn", function() {
-	$(this).parent().siblings().children().css("color", "#fff");
-	$(this).css("background-color", "#5b6ceb");
-});
+    $(document).on("click", ".mobail-menu__close", function () {
+        $(".mobail-menu-container").animate({left:"-100%"}, 500);
+    });
 
+    function openMenu(){
+    	$(".mobail-menu-container").css("display", "block");
+    }
 
+    function closeMenu(){
+    	$(".mobail-menu-container").css("display", "none");
+    }
 
+    if($(window).width() < 768){
+    	openMenu();
+    }else if($(window).width() > 768){
+    	closeMenu();
+    }
 
+    $(window).resize(function () {
+    	if($(window).width() < 768){
+	    	openMenu();
+	    	$(".mobail-menu-container").css("left", "-100%");
+	    }else if($(window).width() > 768){
+	    	closeMenu();
+	    }
+    });
 
-	
 });
 
 function validate() {
-		var name_input = document.forms['forms_balance']['name_input'].value;
-		var phone_input = document.forms['forms_balance']['phone_input'].value;
-
-		if (name_input.length == 0) {
-			alert("заполните обязательное поле")
-			return false;
-		}
-
-		if (phone_input.length == 0) {
-			alert("заполните обязательное поле")
-			return false;
-		}
-
-		var a = phone_input.indexOf("+");
-		var b = phone_input.indexOf("0");
-		var c = phone_input.indexOf("-");
-
-		if (a<1 || b<1 || c<0) {
-			alert("вы  ввели не существующий номер");
-			return false;
-		}
+	var name_input = document.forms['forms_balance']['name_input'].value;
+	var phone_input = document.forms['forms_balance']['phone_input'].value;
+	if (name_input.length == 0) {
+		alert("заполните обязательное поле")
+		return false;
 	}
+	if (phone_input.length == 0) {
+		alert("заполните обязательное поле")
+		return false;
+	}
+}
